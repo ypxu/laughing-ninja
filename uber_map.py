fd0fe72d4d1c0fa57c08e9fcd0a15db705c3a72b
Yingpeng Xu <nelson0235@gmail.com>
2014-07-11 22:15:56 -0700

    Refactor redis initialization

--- uber_map.py
+++ uber_map.py
@@ -21,41 +21,64 @@ cmd = nextbus.Command()
 client = Redis()
 geo_cli = GeoHasher(redis_client=client, lookup_key='stops')
 
+AGENCY_KEY = 'agency'
+ROUTE_KEY = 'route'
+STOP_KEY = 'stop'
+
 
 def init_redis():
+    '''initialize geo lookup in redis'''
+
     app.logger.debug('Initialize redis from nextbus.')
+
     agency_list = cmd.get_agency_list()
     app.logger.debug('### Add %s agency' % len(agency_list))
 
-    agency_key = 'agency'
     for agency in agency_list:
-        if not agency or client.sismember(agency_key, agency.tag):
+        agency_key = '%s:%s' % (AGENCY_KEY, agency.tag)
+        # Check if already process this agency
+        if client.sismember(AGENCY_KEY, agency_key):
             continue
         routes = cmd.get_route_config(agency.tag)
         app.logger.debug('### Add %s agency, %s routes' % (
             agency.tag, len(routes)))
+
         for route in routes:
             init_route(agency, route)
-        client.set('%s:%s' % (agency_key, agency.tag), json.dumps(
+
+        # Store Agency's json dumps
+        client.set(agency_key, json.dumps(
             agency, cls=CustomEncoder))
-        client.sadd(agency_key, agency.tag)
+        client.sadd(AGENCY_KEY, agency_key)
+
     app.logger.debug('Done initialization redis.')
 
 
-def init_route(agency, route, route_key='route', stop_key='stop'):
-    if not route or client.sismember(route_key, route.tag):
+def init_route(agency, route):
+    '''initialize route'''
+
+    route_key = '%s:%s:%s' % (ROUTE_KEY, agency.tag, route.tag)
+    # Check if already process this route
+    if client.sismember(ROUTE_KEY, route_key):
         return
+
     stops = route.stops
     app.logger.debug('### Add %s agency, %s routes, %s stops' % (
         agency.tag, route.tag, len(stops)))
+
     for stop in stops:
-        stop_k = '%s:%s:%s:%s' % (stop_key, agency.tag, route.tag, stop.tag)
-        client.sadd(stop_k, stop.tag)
-        client.set(stop_k, json.dumps(stop, cls=CustomEncoder))
-        geo_cli.add_coordinate(stop.lat, stop.lon, stop_k)
-    client.set('%s:%s:%s' % (route_key, agency.tag, route.tag),
-        json.dumps(route, cls=CustomEncoder))
-    client.sadd(route_key, route.tag)
+        stop_key = '%s:%s:%s:%s' % (STOP_KEY, agency.tag, route.tag, stop.tag)
+        # Check if already process this stop
+        if client.sismember(STOP_KEY, stop_key):
+            continue
+        # Store Stop's json dumps
+        client.sadd(STOP_KEY, stop_key)
+        client.set(stop_key, json.dumps(stop, cls=CustomEncoder))
+        # Add Stop's lat/lon into sorted set
+        geo_cli.add_coordinate(stop.lat, stop.lon, stop_key)
+    # Store Route's json dumps
+    client.sadd(ROUTE_KEY, route_key)
+    client.set(route_key, json.dumps(route, cls=CustomEncoder))
 
 
 def setup_logging():
@@ -86,8 +109,8 @@ def route():
 
 @app.route("/api/route_config.json")
 def route_config():
-    agency, route = (request.args.get('agency'),
-        request.args.get('route'))
+    agency, route = request.args.get('agency'), \
+        request.args.get('route')
     data = cmd.get_route_config(agency, route)
     return flask.jsonify(result=data)
 
@@ -100,7 +123,7 @@ def stop_config():
         stop = json.loads(client.get(stop_key) or {})
         _, agency_tag, route_tag, stop_tag = stop_key.split(':')
         predictions = cmd.get_predictions_by_stopTag(agency_tag,
-            stop_tag, route_tag)
+                                                     stop_tag, route_tag)
         if len(predictions) > 0:
             stop['agencyTitle'] = predictions[0].agencyTitle
             stop['routeTitle'] = predictions[0].routeTitle
@@ -118,8 +141,8 @@ def stop_config():
 
 @app.route("/api/near_routes.json")
 def near_routes():
-    lat, lon, rad = (request.args.get('lat', ''),
-        request.args.get('lon', ''), request.args.get('radius', ''))
+    lat, lon, rad = request.args.get('lat', ''), \
+        request.args.get('lon', ''), request.args.get('radius', '')
     return flask.jsonify(result=geo_cli.query_by_radius(lat, lon, rad))
 
 
@@ -138,11 +161,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run transit app')
     parser.add_argument('--debug', action='store_true',
-      help="debug mode", required=False)
+                        help="debug mode", required=False)
     parser.add_argument('--host', action='store',
-      help="host", required=False, default='0.0.0.0')
+                        help="host", required=False, default='0.0.0.0')
     parser.add_argument('--port', action='store', type=int,
-      help="port", required=False, default=5000)
+                        help="port", required=False, default=5000)
 
     options = parser.parse_args()
 
@@ -153,7 +176,7 @@ if __name__ == '__main__':
     setup_logging()
 
     app.run(host=options.host, port=options.port,
-      debug=options.debug)
+            debug=options.debug)
 
 else:
     # Deploy with Gunicorn (Production)
